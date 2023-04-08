96254dc1a95ab0a264fc2586dcd6b663b65adb8f
h3avy-knight <rohityadavsiri@gmail.com>
2023-04-09 00:08:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("TODOS", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value && value.trim()) {
+      const todo = {
+        id: crypto.randomUUID(),
+        text: value,
+        completed: false,
+      };
+      setTodos([...todos, todo]);
+    }
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const checkedTodos = (id) => {
+    const checked = todos.map((todo) => {
+      if (todo.id == id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(checked);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          maxLength={12}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.checked}
+            onChange={() => checkedTodos(todo.id)}
+          />
+          {todo.text}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
