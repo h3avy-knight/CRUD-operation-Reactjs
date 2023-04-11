265c86301689623e46587775f407384c18fb1d24
h3avy-knight <rohityadavsiri@gmail.com>
2023-04-11 17:07:00 +0530

    add local storage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [value, setValue] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("TODOS", JSON.stringify(todos));
+    const getTodos = localStorage.getItem("todos");
+    let loadedTodos = JSON.parse(getTodos);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(todos);
+    localStorage.setItem("todos", json);
   }, [todos]);
 
   const handleSubmit = (e) => {
@@ -19,6 +30,7 @@ const App = () => {
         completed: false,
       };
       setTodos([...todos, todo]);
+      setValue("");
     }
   };
 
@@ -39,26 +51,45 @@ const App = () => {
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={{ marginTop: "20px" }}>
         <input
           type="text"
-          maxLength={12}
+          placeholder="Max Length is 20"
+          maxLength={20}
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
         <button type="submit">Add</button>
       </form>
       {todos.map((todo) => (
-        <div key={todo.id}>
+        <div style={{ marginTop: "20px" }} key={todo.id}>
           <input
             type="checkbox"
             checked={todo.checked}
             onChange={() => checkedTodos(todo.id)}
           />
+
           {todo.text}
           <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>
       ))}
+      {/* <details>
+        <summary>Developers Community</summary>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores cum
+          necessitatibus atque, architecto aperiam minima recusandae eveniet,
+          nulla sequi, est ad commodi exercitationem temporibus. Nihil
+          voluptates delectus quam cumque possimus officiis ipsa molestiae
+          tempora corrupti doloribus ex, aut vel nisi eveniet sint laudantium
+          numquam enim officia praesentium dolorem. Blanditiis minus, dolor
+          exercitationem nihil architecto, et ut eaque aliquam quia facilis modi
+          ullam, suscipit molestias. Iusto laudantium eum totam dicta, debitis
+          tempore soluta nisi quo pariatur modi delectus molestiae, doloribus
+          asperiores hic voluptatum quasi iste quam eveniet nam tempora illo
+          praesentium assumenda. Dolorum nobis dolore molestias aliquam fugiat
+          adipisci asperiores iusto?
+        </p>
+      </details> */}
     </div>
   );
 };
